53f05b2cc2d77f44c248c7ed3b7b4708df9e73df
majdarbash <majd.arbash@gmail.com>
2018-01-19 12:44:24 +0400

    *[PHP.ini]: some essential configurations +[Recipe]: installing npm, node, bower and grunt

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -5,6 +5,23 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
+%w[
+    /usr/local/scripts
+    /usr/local/scripts/files
+].each do |path|
+    directory path do
+        action :create
+    end
+end
+
+bash 'apt-update' do
+    code <<-EOH
+        apt update
+
+        touch /usr/local/scripts/files/apt-update-done
+    EOH
+    creates '/usr/local/scripts/files/apt-update-done'
+end
 
 package ['nginx'] do
   action :install
@@ -18,7 +35,16 @@ package ['php-imagick', 'php-curl', 'php-gd', 'php-mcrypt', 'php-xml', 'php-mbst
     action :install
 end
 
-package ['ntp', 'htop', 'vim'] do
+template '/etc/php/7.0/fpm/php.ini' do
+  source 'php.ini'
+end
+
+template '/etc/php/7.0/cli/php.ini' do
+  source 'php.ini'
+end
+
+
+package ['ntp', 'htop', 'vim', 'wget', 'mutt', 'npm', 'zip'] do
     action :install
 end
 
@@ -26,8 +52,24 @@ package ['redis-server'] do
     action :install
 end
 
-
-
 template '/etc/nginx/sites-enabled/default' do
   source 'default.conf'
+end
+
+bash 'other-configurations' do
+    code <<-EOH
+        echo "Installing node modules ..."
+        ln /usr/bin/nodejs /usr/bin/node -s
+        npm -g install grunt --unsafe-perm
+        npm install -g bower
+        npm install -g phantomjs-prebuilt --unsafe-perm
+
+        echo "Installing Composer ..."
+        cd /tmp
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php ./composer-setup.php --install-dir=/bin --filename=composer
+
+        touch /usr/local/scripts/files/other-configurations-done
+    EOH
+    creates '/usr/local/scripts/files/other-configurations-done'
 end
